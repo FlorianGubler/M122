448781fc85e9206a56f69dc968793925c617005f
floriangubler <gubler.florian@gmx.net>
2023-07-05 18:45:33 +0000

    Add NBA Api

--- scripts/modules/api/football.py
+++ scripts/modules/api/football.py
@@ -1,15 +1,16 @@
 import requests
 import logging
 
-def loaddata(league, season, url, apikey):
+def loaddata(league, season, url, host, apikey):
   querystring = {"league":league,"season":season}
   headers = {
-    "X-RapidAPI-Key": apikey
+    "X-RapidAPI-Key": apikey,
+    "X-RapidAPI-Host": host
   }
   response = requests.get(url, headers=headers, params=querystring)
   if response.status_code == 200:
     logging.info("Called Football API succesfully")
     return response.json()
   else:
-    logging.error("Football API Call failed - Response: " + str(response)) 
+    logging.error("Football API Call failed - Response: '" + response.text + "'") 
     raise Exception("Football API Call failed (StatusCode: " + str(response.status_code) + ")")
--- scripts/modules/api/nba.py
+++ scripts/modules/api/nba.py
@@ -1,15 +1,16 @@
 import requests
 import logging
 
-def loaddata(season, url, apikey):
+def loaddata(season, url, host, apikey):
   querystring = {"season":season}
   headers = {
     "X-RapidAPI-Key": apikey,
+    "X-RapidAPI-Host": host
   }
   response = requests.get(url, headers=headers, params=querystring)
   if response.status_code == 200:
     logging.info("Called NBA API succesfully")
     return response.json()
   else:
-    logging.error("NBA API Call failed - Response: " + str(response))
+    logging.error("NBA API Call failed - Response: '" + response.text + "'")
     raise Exception("NBA API Call failed (StatusCode: " + str(response.status_code) + ")")
