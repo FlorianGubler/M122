6854ed3c95a26ecb1a69cc26ce6f8935bf78c2e8
Florian Gubler <70200329+FlorianGubler@users.noreply.github.com>
2023-07-06 16:40:52 +0200

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -23,17 +23,22 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=LOG_LEVEL,
     datefmt='%Y-%m-%d %H:%M:%S')
-# Script
-logging.info("Setup finished - Starting script")
-## Get API Data
-APIDATA = {}
-### FOOTBALL API
-APIDATA['football'] = modules.api.football.loaddata(CONFIG['api']['football'], datetime.date.today().year, CONFIG['cred']['apikeys']['football'])
-### NBA API
-APIDATA['nba'] = modules.api.nba.loaddata(CONFIG['api']['nba'], CONFIG['cred']['apikeys']['nba'])
-### Generate PDF Report
-modules.pdf.reportPDF.generatePDFReport(CONFIG['pdf'], APIDATA);
-### Send Mail Report
-modules.mail.manageMail.send_email(CONFIG['smtp'], CONFIG['pdf']['tmpfile'], CONFIG['cred']['mail']['password'])
-### Send Report via FTP to Server
-modules.ftp.sendFTP.send_ftp(CONFIG['ftp'], CONFIG['pdf']['tmpfile'], CONFIG['cred']['ftp']['password'])
+    
+## SCRIPT
+try:
+    logging.info("Setup finished - Starting script")
+    ## Get API Data
+    APIDATA = {}
+    ### FOOTBALL API
+    APIDATA['football'] = modules.api.football.loaddata(CONFIG['api']['football'], datetime.date.today().year, CONFIG['cred']['apikeys']['football'])
+    ### NBA API
+    APIDATA['nba'] = modules.api.nba.loaddata(CONFIG['api']['nba'], CONFIG['cred']['apikeys']['nba'])
+    ### Generate PDF Report
+    modules.pdf.reportPDF.generatePDFReport(CONFIG['pdf'], APIDATA);
+    ### Send Mail Report
+    modules.mail.manageMail.send_email(CONFIG['smtp'], CONFIG['pdf']['tmpfile'], CONFIG['cred']['mail']['password'])
+    ### Send Report via FTP to Server
+    modules.ftp.sendFTP.send_ftp(CONFIG['ftp'], CONFIG['pdf']['tmpfile'], CONFIG['cred']['ftp']['password'])
+except Exception, e:
+    logging.error("An Exception occured: " + str(e))
+    modules.mail.manageMail.send_error_email(CONFIG['smtp'], str(e), CONFIG['cred']['mail']['password'])
