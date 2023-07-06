e001f17c9d03acd78dd780f534f3675a60f9eb26
Florian Gubler <70200329+FlorianGubler@users.noreply.github.com>
2023-07-06 09:44:11 +0200

    Update football.py

--- scripts/modules/api/football.py
+++ scripts/modules/api/football.py
@@ -4,8 +4,7 @@ import logging
 def loaddata(config, season, apikey):
   querystring = {"league":config['properties']['league'],"season":season}
   headers = {
-    "X-RapidAPI-Key": apikey,
-    "X-RapidAPI-Host": config['host']
+    "X-RapidAPI-Key": apikey
   }
   response = requests.get(config['url'], headers=headers, params=querystring)
   if response.status_code == 200:
